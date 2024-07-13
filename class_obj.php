a31358ac12204310ee20494c38872c464e152d49
Sanjana Kumbhar <120375747+sanju1906@users.noreply.github.com>
2024-07-13 12:23:01 +0530

    Update class_obj.php

--- class_obj.php
+++ class_obj.php
@@ -1,13 +1,16 @@
 class Animals{
-    private $color;
-    private $name;
+    private $color;  //data members
+    private $name;   
     public function __construct($color, $name){
        $this->color = $color;
        $this->name = $name;
-    }
+    } // member function
     public function data (){
     echo "Animal name is ".$this->name." and its color is ".$this->color;
     }
 }
 $ani1 = new Animals("white","Rabbit");
 $ani1->data();
+
+
+//encapsulation -object-oriented programming to bundle data and methods into easy-to-use units. eg. methods and variables in class
